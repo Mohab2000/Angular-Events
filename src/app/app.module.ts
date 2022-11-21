8cf3d99495d334577f5cc6acbacfff882e2654a0
Mohab2000 <mohabahmed20005@gmail.com>
2022-11-21 16:23:49 +0200

    create event routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./common/toastr.service";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { appRoutes } from "./routes";
+import { CreateEventComponent } from "./events/create-event.component";
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
 
@@ -18,6 +19,7 @@ import { appRoutes } from "./routes";
     EventThumbnailComponent,
     EventDetailsComponent,
     NavBarComponent,
+    CreateEventComponent,
   ],
   providers: [EventService, ToastrService],
 
