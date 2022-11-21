7478688f709ff0093e98cc44e3db591efc34f427
Mohab2000 <mohabahmed20005@gmail.com>
2022-11-21 13:07:32 +0200

    creating services and third parties done

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,8 +1,7 @@
-import { toBase64String } from "@angular/compiler/src/output/source_map";
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "./shared/event.service";
 import { ToastrService } from "../common/toastr.service";
-declare let toastr;
+
 @Component({
   selector: "events-list",
   template: `
