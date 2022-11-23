ea665c4d9c5a3b89b5d4dc6549553b298b801350
Mohab2000 <mohabahmed20005@gmail.com>
2022-11-23 13:04:48 +0200

    validation for log in

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,10 +4,20 @@ import { AuthService } from "./auth.service";
 
 @Component({
   templateUrl: "./login.component.html",
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+    `,
+  ],
 })
 export class LoginComponent {
   userName: any;
   password: any;
+  mouseoverLogin;
   constructor(private authService: AuthService, private router: Router) {}
   login(formValues) {
     this.authService.loginUser(formValues.userName, formValues.password);
