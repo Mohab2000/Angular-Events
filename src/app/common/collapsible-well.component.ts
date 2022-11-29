b478eaa78f0b2e42b45c3f2e965f3d4aa8f33d1d
Mohab2000 <mohabahmed20005@gmail.com>
2022-11-29 14:46:23 +0200

    title removed as input

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -10,7 +10,6 @@ import { Component, Input } from "@angular/core";
   `,
 })
 export class CollapsibleWellComponent {
-  @Input() title: string;
   visible: boolean = true;
   toggleContent() {
     this.visible = !this.visible;
